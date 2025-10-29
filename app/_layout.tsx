6b3c021da12c3c7f1e1fb58b9dabbd27a20ac7e5
toranbehera <toranbehera@gmail.com>
2025-10-29 17:03:15 +0700

    testing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/components/useColorScheme';
-
+//
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
